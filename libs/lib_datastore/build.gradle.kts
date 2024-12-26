d2238f6215912cfcabda6b0f8307b8bbff97da6c
beny <liantriana.beny@gmail.com>
2024-12-26 21:39:35 +0700

    implement datastore

--- libs/lib_datastore/build.gradle.kts
+++ libs/lib_datastore/build.gradle.kts
@@ -1,12 +1,31 @@
 plugins {
     id("jp.speakbuddy.lib-convention")
+    alias(libs.plugins.protobuf)
 }
 
 android {
     namespace = "jp.speakbuddy.lib_datastore"
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.22.0"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                val java by registering {
+                    option("lite")
+                }
+                val kotlin by registering {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation(libs.datastore)
     implementation(libs.datastore.preferences)
+    implementation(libs.protobuf.kotlin.lite)
 }
---
+++ libs/lib_datastore/src/main/java/jp/speakbuddy/lib_datastore/di/ModuleInjection.kt
@@ -0,0 +1,32 @@
+package jp.speakbuddy.lib_datastore.di
+
+import FactSerializer
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import jp.speakbuddy.lib_datastore.FactPreference
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ModuleInjection {
+    companion object {
+        private const val FACT_PREFERENCE_NAME = "fact_preference.pb"
+    }
+
+    @Provides
+    @Singleton
+    fun provideProtoDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<FactPreference> = DataStoreFactory.create(
+        serializer = FactSerializer
+    ) {
+        context.dataStoreFile(FACT_PREFERENCE_NAME)
+    }
+}
