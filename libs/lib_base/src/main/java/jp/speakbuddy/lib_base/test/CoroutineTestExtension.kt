dea7b224a25f4d606d49f1978bebb0388062a2c3
beny <liantriana.beny@gmail.com>
2025-01-08 15:50:49 +0700

    clean up on lib module

--- libs/lib_base/src/main/java/jp/speakbuddy/lib_base/test/CoroutineTestExtension.kt
+++ libs/lib_base/src/main/java/jp/speakbuddy/lib_base/test/CoroutineTestExtension.kt
@@ -13,6 +13,9 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 
+/**
+ * Unit test revert back using Junit4 because robolectric still doesn't support Junit5
+ */
 @OptIn(ExperimentalCoroutinesApi::class)
 open class CoroutineTestExtension(unconfined: Boolean = false) {
     private val scope: TestScope = TestScope()
